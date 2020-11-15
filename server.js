95a7904dbc5a9539fd0ee3cd96011fa4ba994706
Bhagabati Prasad <bhagabatiprasada@gmail.com>
2020-11-15 20:25:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidV4 } = require("uuid");
 
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -26,4 +26,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(4000, () => console.log("Listening on port 4000"));
+server.listen(3000);
